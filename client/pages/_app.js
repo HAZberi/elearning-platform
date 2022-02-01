fab607d979337e5172ac02d9c9f414606fe7e737
Hassaan Zuberi <mhazuberi@gmail.com>
2022-02-01 14:19:59 +0300

    implemented basic app bar component fork from docs

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/utils/createEmotionCache';
+import Header from '../src/components/HeaderNavigation';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -21,6 +22,7 @@ export default function MyApp(props) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
+        <Header />
         <Component {...pageProps} />
       </ThemeProvider>
     </CacheProvider>
