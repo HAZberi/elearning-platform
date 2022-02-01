ef9535e6e9b562ca71b77d67b1efbfd803833d37
Hassaan Zuberi <mhazuberi@gmail.com>
2022-02-01 13:29:38 +0300

    added Box styles to Login and Register.

--- client/pages/login.js
+++ client/pages/login.js
@@ -1,11 +1,12 @@
 import * as React from "react";
 import Container from "@mui/material/Container";
 import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
 
 const Login = () => {
   return (
     <Container maxWidth="sm">
-      <Box sx={{ my: 4 }}>
+      <Box sx={{ my: 4, textAlign: "center" }}>
         <Typography variant="h2" component="h1" gutterBottom>
           Login
         </Typography>
