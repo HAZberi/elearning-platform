86773f92fd670b3814acd6fec723fa7b45f9315b
Hassaan Zuberi <mhazuberi@gmail.com>
2022-02-17 02:22:38 +0300

    modified register method to send jwt cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,18 +1,45 @@
+const jwt = require('jsonwebtoken');
+//const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/userModel');
 
-exports.register = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
-
-  //Filter newUser data to create a customized api response
-  const { name, email, createdAt } = newUser;
-
-  res.status(201).json({
-    status: 'success',
-    data: {
-      name,
-      email,
-      createdAt,
-    },
+const signToken = (id) => {
+  const token = jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
   });
+  return token;
+};
+
+const createSendToken = (user, statusCode, res) => {
+  const token = signToken(user._id);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  //----------If Server is listening from a secure protocol HTTPS-----------//
+  // if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
+  res.status(statusCode).json({
+    status: 'success',
+    data: user,
+  });
+};
+
+exports.register = catchAsync(async (req, res, next) => {
+  const { name, email, password } = req.body;
+  const newUser = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  createSendToken(newUser, 201, res);
 });
